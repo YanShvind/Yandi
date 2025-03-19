11547549e5121b6ef62a59d6bb763671f04395d9
Ян Швындиков <Yansv2006@gmail.com>
2025-03-19 18:04:13 +0300

    Add TabBar

--- Yandi/Controllers/TabBarViewController.swift
+++ Yandi/Controllers/TabBarViewController.swift
@@ -7,12 +7,57 @@
 
 import UIKit
 
-final class TabBarViewController: UIViewController {
+final class TabBarViewController: UITabBarController {
 
-    override func viewDidLoad() {
+        override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemBackground
+        self.view.backgroundColor = .systemRed
+        self.tabBar.backgroundImage = UIImage()
+        self.tabBar.backgroundColor = .secondarySystemBackground
+        setUpTabs()
+    }
+    
+    private func setUpTabs() {
+        let targetVC = TargetViewController()
+        let notesVC = NotesViewController()
+        let diaryVC = DiaryViewController()
+        let friendsVC = FriendsViewController()
+        let profileVC = ProfileViewController()
+        
+        targetVC.navigationItem.largeTitleDisplayMode = .automatic
+        notesVC.navigationItem.largeTitleDisplayMode = .automatic
+        diaryVC.navigationItem.largeTitleDisplayMode = .automatic
+        friendsVC.navigationItem.largeTitleDisplayMode = .automatic
+        profileVC.navigationItem.largeTitleDisplayMode = .automatic
+        
+        let nav1 = UINavigationController(rootViewController: targetVC)
+        let nav2 = UINavigationController(rootViewController: notesVC)
+        let nav3 = UINavigationController(rootViewController: diaryVC)
+        let nav4 = UINavigationController(rootViewController: friendsVC)
+        let nav5 = UINavigationController(rootViewController: profileVC)
+        
+        nav1.tabBarItem = UITabBarItem(title: "Target",
+                                       image: UIImage(systemName: "house"),
+                                       tag: 1)
+        nav2.tabBarItem = UITabBarItem(title: "Notes",
+                                       image: UIImage(systemName: "magnifyingglass"),
+                                       tag: 2)
+        nav3.tabBarItem = UITabBarItem(title: "Diary",
+                                       image: UIImage(systemName: "bookmark.fill"),
+                                       tag: 3)
+        nav4.tabBarItem = UITabBarItem(title: "Friends",
+                                       image: UIImage(systemName: "heart.fill"),
+                                       tag: 4)
+        nav5.tabBarItem = UITabBarItem(title: "Profile",
+                                       image: UIImage(systemName: "plus"),
+                                       tag: 5)
+        
+        for nav in [nav1, nav2, nav3, nav4, nav5] {
+            nav.navigationBar.scrollEdgeAppearance = nav.navigationBar.standardAppearance
+        }
+        
+        setViewControllers([nav1, nav2, nav3, nav4, nav5], animated: true)
     }
 }
 
